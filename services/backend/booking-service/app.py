171f9d8be23ba46df354768927f947452f340547
paylays <10221081@student.itk.ac.id>
2025-04-30 22:36:32 +0800

    add cors origin

--- services/backend/booking-service/app.py
+++ services/backend/booking-service/app.py
@@ -2,12 +2,18 @@ from flask import Flask
 from config import Config
 from database.db import db
 from routes.booking_routes import booking_bp
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
 db.init_app(app)
-app.register_blueprint(booking_bp, url_prefix='/booking')
+CORS(app, resources={r"/*": {"origins": "http://localhost:5173"}})
+
+with app.app_context():
+    db.create_all()
+
+app.register_blueprint(booking_bp)
 
 if __name__ == '__main__':
     app.run(port=5003, debug=True, host='0.0.0.0')
