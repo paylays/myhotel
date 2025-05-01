02387bd43d04774b2a213fc9f7fb0a7323eb01b3
paylays <10221081@student.itk.ac.id>
2025-05-02 02:37:14 +0800

    add cors for 2 port and include jwt manager

--- services/backend/booking-service/app.py
+++ services/backend/booking-service/app.py
@@ -3,12 +3,18 @@ from config import Config
 from database.db import db
 from routes.booking_routes import booking_bp
 from flask_cors import CORS
+from flask_jwt_extended import JWTManager
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
 db.init_app(app)
-CORS(app, resources={r"/*": {"origins": "http://localhost:5173"}})
+jwt = JWTManager(app)
+jwt.init_app(app)
+CORS(app, resources={r"/*": {"origins": [
+    "http://localhost:5173",
+    "http://localhost:5174"
+]}}, supports_credentials=True, expose_headers="Authorization", allow_headers=["Content-Type", "Authorization"])
 
 with app.app_context():
     db.create_all()
