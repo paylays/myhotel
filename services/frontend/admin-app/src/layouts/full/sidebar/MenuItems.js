e888c07da1f0ebde4cd50d77afd1e2b76eec4334
paylays <10221081@student.itk.ac.id>
2025-05-02 11:47:29 +0800

    delete some menu

--- services/frontend/admin-app/src/layouts/full/sidebar/MenuItems.js
+++ services/frontend/admin-app/src/layouts/full/sidebar/MenuItems.js
@@ -1,9 +1,7 @@
-
 import { uniqueId } from 'lodash';
 
 import {
-  IconCopy, IconLayoutDashboard, IconTypography, IconUserCircle, IconLogin, 
-  IconAlignBoxLeftBottom, IconCheckbox, IconRadar, IconSlideshow, IconCaretUpDown, IconTable, IconForms,
+  IconLayoutDashboard,
   IconBed, IconCalendarCheck
 } from '@tabler/icons-react';
 
@@ -36,85 +34,7 @@ const Menuitems = [
     title: 'Booking Management',
     icon: IconCalendarCheck,
     href: '/bookings',
-  },
-  {
-    id: uniqueId(),
-    title: 'Autocomplete',
-    icon: IconTypography,
-    href: '/form-elements/autocomplete',
-  },
-  {
-    id: uniqueId(),
-    title: 'Buttons',
-    icon: IconAlignBoxLeftBottom,
-    href: '/form-elements/button',
-  },
-  {
-    id: uniqueId(),
-    title: 'Checkbox',
-    icon: IconCheckbox,
-    href: '/form-elements/checkbox',
-  },
-  {
-    id: uniqueId(),
-    title: 'Radio',
-    icon: IconRadar,
-    href: '/form-elements/radio',
-  },
-  {
-    id: uniqueId(),
-    title: 'Slider',
-    icon: IconSlideshow,
-    href: '/form-elements/slider',
-  },
-  {
-    id: uniqueId(),
-    title: 'Switch',
-    icon: IconCaretUpDown,
-    href: '/form-elements/switch',
-  },
-  {
-    id: uniqueId(),
-    title: 'Tables',
-    icon: IconTable,
-    href: '/tables/basic-table',
-  },
-  {
-    id: uniqueId(),
-    title: 'Form Layouts',
-    icon: IconForms,
-    href: '/form-layouts',
-  },
-  {
-    id: uniqueId(),
-    title: 'Typography',
-    icon: IconTypography,
-    href: '/ui/typography',
-  },
-  {
-    id: uniqueId(),
-    title: 'Shadow',
-    icon: IconCopy,
-    href: '/ui/shadow',
-  },
- 
-  {
-    navlabel: true,
-    subheader: 'Auth',
-  },
-  {
-    id: uniqueId(),
-    title: 'Login',
-    icon: IconLogin,
-    href: '/login',
-  },
-  {
-    id: uniqueId(),
-    title: 'Register',
-    icon: IconUserCircle,
-    href: '/auth/register',
-  },
-  
+  },  
 ];
 
 export default Menuitems;
